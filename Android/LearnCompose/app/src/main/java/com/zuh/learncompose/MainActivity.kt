de2c702ded34816c007dc25ed185522f0877c88b
Tran Vu An <vuan.tran@zuhlke.com>
2022-01-12 21:01:30 +0700

    Compose modifier

--- Android/LearnCompose/app/src/main/java/com/zuh/learncompose/MainActivity.kt
+++ Android/LearnCompose/app/src/main/java/com/zuh/learncompose/MainActivity.kt
@@ -3,11 +3,13 @@ package com.zuh.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.zuh.learncompose.ui.theme.LearnComposeTheme
 
@@ -16,10 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearnComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                Greeting("Android")
             }
         }
     }
@@ -27,7 +26,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Hello $name!")
+    }
 }
 
 @Preview(showBackground = true)
