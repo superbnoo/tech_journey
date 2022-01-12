e2f68d90175c8010d6aedee693b260a30350a49c
Tran Vu An <vuan.tran@zuhlke.com>
2022-01-12 21:24:23 +0700

    Custom Text

--- Android/LearnCompose/app/src/main/java/com/zuh/learncompose/MainActivity.kt
+++ Android/LearnCompose/app/src/main/java/com/zuh/learncompose/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.zuh.learncompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,31 +12,38 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.zuh.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearnComposeTheme {
-                Greeting("Android")
-            }
+            Title()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Hello $name!")
-    }
+fun Title() {
+    val context = LocalContext.current
+    Text(text = "Tran Vu An",
+        fontSize = 32.sp, color = Color.White,
+        fontFamily = FontFamily.Cursive,
+        modifier = Modifier.clickable {
+            Toast.makeText(context, "Hello", Toast.LENGTH_LONG).show()
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    LearnComposeTheme {
-        Greeting("Android")
+    Surface(modifier = Modifier.fillMaxSize(), color = Color.Black) {
+        Title()
     }
 }
